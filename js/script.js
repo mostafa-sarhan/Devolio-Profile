fba41488295b419bb16248aa61939a5ae08145b6
mostafa-sarhan <122050594+mostafa-sarhan@users.noreply.github.com>
2024-05-04 14:24:45 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+    // const box1 = document.getElementById('box1');
+    // const box2 = document.getElementById('box2');
+    // let currentBox = 1; 
+
+    // setInterval(() => {
+    //     if (currentBox === 1) {
+    //         setTimeout(() => {
+    //             box1.classList.remove('active');
+
+    //         },2000)
+    //         box2.classList.add('active');
+    //     } else {
+    //         box2.classList.remove('active');
+    //         box1.classList.add('active');
+    //     }
+
+    //     currentBox = currentBox === 1 ? 2 : 1;
+
+    //     // setTimeout(() => {
+    //     //     if (currentBox === 1) {
+    //     //         box1.classList.add('active');
+    //     //     } else {
+    //     //         box2.classList.add('active');
+    //     //     }
+    //     // }, );
+    // }, 3000); 
